6b214bc6d7812c5e67be3fffd7a4e09ca6a052cb
jkwon-dev <kwonejk@gmail.com>
2023-12-23 16:14:46 +0900

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by kwon eunji on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
